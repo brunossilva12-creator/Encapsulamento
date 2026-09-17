ef45b6b5c9e1c73d7e06e533c00cdc7bfcfb56df
brunossilva12-creator <bruno.s.silva12@edu.senai.br>
2026-09-17 07:47:10 -0300

    Encapsulamento + pequenas correções

---
+++ Carro.cs
@@ -0,0 +1,29 @@
+﻿class Carro
+{
+    private int _velocidadeAtual;
+
+    public int VelocidadeAtual
+    {
+        get
+        {
+            return _velocidadeAtual;
+        }
+    }
+
+    public void Acelerar()
+    {
+        _velocidadeAtual += 10;
+    }
+
+    public void Frear()
+    {
+        if (_velocidadeAtual >= 10)
+        {
+            _velocidadeAtual -= 10;
+        }
+        else
+        {
+            _velocidadeAtual = 0;
+        }
+    }
+}
